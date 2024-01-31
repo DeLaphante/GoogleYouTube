fe551b6f97c0d37bb949204148ee5581508b2a18
Question Everything <boygadget@hotmail.com>
2024-01-31 20:24:23 +0000

    handle mobile view

--- GoogleYouTube/PageObjects/Google/GoogleHomePage.cs
+++ GoogleYouTube/PageObjects/Google/GoogleHomePage.cs
@@ -15,8 +15,8 @@ namespace GoogleYouTube.PageObjects.CommonPages
 
         #region Locators
 
-        PageElement Search_textbox => new PageElement(_Driver, By.XPath($"//textarea[@title='Search']"));
-        PageElement Results_label => new PageElement(_Driver, By.XPath($"//div[@id='search']//div[@jscontroller and @lang]"));
+        PageElement Search_textbox => new PageElement(_Driver, By.XPath($"//textarea[@title='Search']|//textarea[@aria-label='Google Search']"));
+        PageElement Results_label => new PageElement(_Driver, By.XPath($"//div[@id='search']//div[@jscontroller and @lang]|//div[@jsdata and @data-hveid and @data-ved and not(@jscontroller)]"));
 
         #endregion
 
@@ -24,6 +24,7 @@ namespace GoogleYouTube.PageObjects.CommonPages
 
         public void Search(string term)
         {
+            Search_textbox.Click();
             Search_textbox.SendKeysNoValidation(term + Keys.Enter);
         }
 
