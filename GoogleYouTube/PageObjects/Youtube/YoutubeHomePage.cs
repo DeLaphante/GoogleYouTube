f703943c7cb5c954d2f43f4ea3c9fa52b1e394dd
Question Everything <boygadget@hotmail.com>
2024-10-11 15:44:58 +0100

    minor change

--- GoogleYouTube/PageObjects/Youtube/YoutubeHomePage.cs
+++ GoogleYouTube/PageObjects/Youtube/YoutubeHomePage.cs
@@ -16,7 +16,7 @@ namespace GoogleYouTube.PageObjects.CommonPages
         #region Locators
 
         PageElement Search_label => new PageElement(_Driver, By.XPath($"//span[text()='Search YouTube']"));
-        PageElement Search_textbox => new PageElement(_Driver, By.XPath($"//input[@id='search' or @name='search']"));
+        PageElement Search_textbox => new PageElement(_Driver, By.XPath($"//input[@id='search' or @name='search' or @placeholder='Search']"));
         PageElement Results_label => new PageElement(_Driver, By.XPath($"//ytd-video-renderer|//ytd-playlist-renderer|//ytm-video-with-context-renderer"));
 
         #endregion
